b86c8aec671cddc7869eb97e3053fb631e82c91d
ashkan <askanfotohi81@gmail.com>
2024-04-19 13:21:41 +0330

    fix some bug

--- src/main/java/com/example/ptmedia/repository/ProfileRepository.java
+++ src/main/java/com/example/ptmedia/repository/ProfileRepository.java
@@ -1,13 +1,8 @@
 package com.example.ptmedia.repository;
 
 import com.example.ptmedia.model.Profile;
-import com.example.ptmedia.service.dto.PostView;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
